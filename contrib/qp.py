b613f12fa63a5974c63ae7a1441f74bf24cc9637
Vince Buffalo <vsbuffaloAAAAAA@gmail.com>
2013-07-25 15:47:12 -0700

    fixed minor issue

--- contrib/qp.py
+++ contrib/qp.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import os
 import re
 import sys
@@ -11,12 +12,14 @@ import pdb
 DEFAULT_SCYTHE_PRIOR = 0.4
 DEFAULT_TRIMFQ_ERROR = 0.05
 
-QUAL_CMD = """
+QUAL_PE_CMD = """\
 pairs join -t -s {in_1} {in_2} \
-| seqqs -e -i -s -p {prefix}_raw - \
+| seqqs -k {k} -e -i -s -p {prefix}_raw - \
 | scythe -a {adapters} -p {prior} - 2> {prefix}_scythe.stderr \
 | seqtk trimfq -q {trim_error} - \
-| seqqs -e -i -s -p {prefix}_trimmed - \
+| seqqs -e -k {k} -i -s -p {prefix}_trimmed - """
+
+FINAL_SPLIT_CMD = """\
 | pairs split -1 {out_1} -2 {out_2} -u {out_unpaired} -
 """
 
@@ -40,8 +43,11 @@ def make_outputfiles(infiles, outext="fastq"):
 def process_pe(args):
     argsdict = dict(in_1=args.in1, in_2=args.in2, adapters=args.a, prior=args.p, trim_error=args.e)
     argsdict.update(make_outputfiles((args.in1, args.in2)))
-    cmd = QUAL_CMD.format(**argsdict)
-    sys.stderr.write("running pipeline\n\t%s\n" % cmd)
+    cmd = QUAL_PE_CMD
+    if args.split:
+       cmd += FINAL_SPLIT_CMD 
+    cmd = cmd.format(**argsdict)
+    sys.stderr.write("\nrunning pipeline:\n\t%s\n" % cmd)
     ret = check_call(cmd, shell=True)
     return ret
     
@@ -50,13 +56,22 @@ def process_se(args):
     pass
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(prog='qp')
+    parser = argparse.ArgumentParser(prog='qp', info="""\
+qp, run a generic quality processing pipeline on short reads. 
+
+Note that seqqs (the program collecting quality information) does not
+hash positional k-mers by default. If you turn this on, it can use a lot of
+memory, so use wish caution. Short k-mers (around 6-8bp) are often
+enough to detect contaminants like barcoes and adapters.""")
     parser.add_argument('-p', type=float, help='scythe prior contaminant rate', default=DEFAULT_SCYTHE_PRIOR)
     parser.add_argument('-e', type=float, help='trimfq error rate threshold', default=DEFAULT_TRIMFQ_ERROR)
+    parser.add_argument('-k', type=int, help='k-mer size for positional k-mer hashing', 
+                        default=0)
     subparsers = parser.add_subparsers(help='sub-command help')
 
     parser_pe = subparsers.add_parser('pe', help='paired-end processing')
     parser_pe.add_argument('-a', type=str, help='adapters FASTA file', required=True)
+    parser_pe.add_argument('-s', action="store_true", help="split interleaved file back into pairs and orphan file")
     parser_pe.add_argument('in1', type=str, help='read pair 1 FASTQ file')
     parser_pe.add_argument('in2', type=str, help='read pair 2 FASTQ file')
     parser_pe.set_defaults(func=process_pe)
